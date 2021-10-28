eb0286179b724a648ddc88aea8c1fab49833c50e
Andrew Simpson <andrew.simpson.x@gmail.com>
2021-10-28 15:09:24 -0600

    Android fixes

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,9 +1,12 @@
 package io.ionic.starter;
 
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 import com.hemangkumar.capacitorgooglemaps.CapacitorGoogleMaps;
 
 public class MainActivity extends BridgeActivity {
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
